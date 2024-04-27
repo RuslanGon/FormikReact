05708b4f8dac728bbe340c8cc322515794ec7eb7
RuslanGon <ruslan441006@gmail.com>
2024-04-27 15:52:26 +0300

    wer

--- src/component/MailBoxForm/MailBoxForm.jsx
+++ src/component/MailBoxForm/MailBoxForm.jsx
@@ -1,36 +1,35 @@
+import { Form, Formik } from "formik";
 
-const MailBoxForm = ({onAddUsers}) => {
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const userEmail = event.currentTarget.elements.userEmail.value
-        const userName = event.currentTarget.elements.userName.value
-        const formData = {
-            userEmail,
-            userName
-        }
-        onAddUsers(formData)
-        event.currentTarget.reset()
-    }
-  return (
-   <form onSubmit={handleSubmit}>
-    <h2>Add new users</h2>
-    <label>
-        <span>User email</span>
-        <br />
-        <input type="email" name="userEmail" placeholder="@email" required />
-    </label>
+const MailBoxForm = ({ onAddUsers }) => {
+  const handleSubmit = () => {
+   
     
-    <br />
-    <label>
-        <span>User name</span>
-        <br />
-        <input type="text" name="userName" placeholder="name" required />
-    </label>
-    <br />
-    <br />
-    <button type="submit"> ▶ Create new users</button>
-   </form>
-  )
-}
+    // onAddUsers(formData);
+    // event.currentTarget.reset();
+  };
 
-export default MailBoxForm
+  return (
+    <Formik initialValues={{}} onSubmit={() => {}}>
+      <Form>
+        <h2>Add new users</h2>
+        <label>
+          <span>User email</span>
+          <br />
+          <input type="email" name="userEmail" placeholder="@email" required />
+        </label>
+
+        <br />
+        <label>
+          <span>User name</span>
+          <br />
+          <input type="text" name="userName" placeholder="name" required />
+        </label>
+        <br />
+        <br />
+        <button type="submit"> ▶ Create new users</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default MailBoxForm;
