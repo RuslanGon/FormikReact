aae5d3806e92041aac58487b3d6c56ac2c53a9f2
RuslanGon <ruslan441006@gmail.com>
2024-04-27 17:46:02 +0300

    wer

--- src/component/MailBoxForm/MailBoxForm.jsx
+++ src/component/MailBoxForm/MailBoxForm.jsx
@@ -1,24 +1,27 @@
 import { ErrorMessage, Field, Form, Formik } from "formik";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 import { MAX_CHAR_NAME_VALIDATION } from "../../utils/constans";
 
- const FORM_INITIAL_VALUES = {
-    userEmail:'',
-    userName:'',
-    favColor: ''
-}
+const FORM_INITIAL_VALUES = {
+  userEmail: "",
+  userName: "",
+  favColor: "",
+};
 
 const mailBosSchema = Yup.object({
-    userEmail: Yup.string()
-    .required('Email adress is reguired')
-    .email('You must enter valid email adress!')
-    .matches(/[^\s]+/, 'not trim'),
-    userName: Yup.string().
-    required('User name is reguired')
-    .max(MAX_CHAR_NAME_VALIDATION, `Your user name must be less than ${MAX_CHAR_NAME_VALIDATION} sumbal`),
-    favColor:Yup.string().
-    required('favourite color is reguired')
-    .oneOf(['Red', 'Green', 'Blui'], "favourite color must be one ")
+  userEmail: Yup.string()
+    .required("Email adress is reguired")
+    .email("You must enter valid email adress!")
+    .matches(/[^\s]+/, "not trim"),
+  userName: Yup.string()
+    .required("User name is reguired")
+    .max(
+      MAX_CHAR_NAME_VALIDATION,
+      `Your user name must be less than ${MAX_CHAR_NAME_VALIDATION} sumbal`
+    ),
+  favColor: Yup.string()
+    .required("favourite color is reguired")
+    .oneOf(["Red", "Green", "Blui"], "favourite color must be one "),
 });
 
 const MailBoxForm = ({ onAddUsers }) => {
@@ -28,42 +31,45 @@ const MailBoxForm = ({ onAddUsers }) => {
   };
 
   return (
-    <Formik initialValues={FORM_INITIAL_VALUES} onSubmit={handleSubmit} validationSchema={mailBosSchema} >
+    <Formik
+      initialValues={FORM_INITIAL_VALUES}
+      onSubmit={handleSubmit}
+      validationSchema={mailBosSchema}
+    >
       <Form>
         <h2>Add new users</h2>
         <label>
           <span>User email</span>
           <br />
           <Field type="email" name="userEmail" placeholder="@email" />
-          <ErrorMessage component='p' name="userEmail" />
+          <ErrorMessage component="p" name="userEmail" />
         </label>
         <br />
         <label>
           <span>User name</span>
           <br />
           <Field type="text" name="userName" placeholder="name" required />
-          <ErrorMessage component='p' name="userName" />
+          <ErrorMessage component="p" name="userName" />
         </label>
         <br />
         <label>
           <span>favourite color</span>
           <br />
           <label>
-          <Field type="radio" name="favColor" value='red' />
-          <span>Red:</span>
+            <Field type="radio" name="favColor" value="red" />
+            <span>Red:</span>
           </label>
           <label>
-          <Field type="radio" name="favColor" value='green' />
-          <span>Green:</span>
+            <Field type="radio" name="favColor" value="green" />
+            <span>Green:</span>
           </label>
           <label>
-          <Field type="radio" name="favColor" value='blue' />
-          <span>Blue:</span>
+            <Field type="radio" name="favColor" value="blue" />
+            <span>Blue:</span>
           </label>
         </label>
         <br />
         <button type="submit"> ▶ Create new users</button>
-       
       </Form>
     </Formik>
   );
